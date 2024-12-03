0be886e1c4985fac0066e82a45659ef3c77eaf39
Alex Popescu <pop.alx@gmail.com>
2024-12-03 18:44:49 +0000

    Working with python-lambda-local and sam

--- lambda/app.py
+++ lambda/app.py
@@ -3,6 +3,11 @@ import os
 import boto3
 from boto3.dynamodb.conditions import Key
 from decimal import Decimal
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Add this custom JSON encoder class
 class DecimalEncoder(json.JSONEncoder):
@@ -12,24 +17,36 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(obj)
 
 def lambda_handler(event, context):
-    # For local testing with DynamoDB Local
-    if 'LOCAL_TESTING' in os.environ:
-        dynamodb = boto3.resource('dynamodb',
-                                endpoint_url='http://localhost:4566',
-                                region_name='us-east-1',
-                                aws_access_key_id='dummy',
-                                aws_secret_access_key='dummy')
-    else:
-        dynamodb = boto3.resource('dynamodb')
-    
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
     try:
+        logger.info('Starting lambda execution')
+        # logger.info(f'Event received: {json.dumps(event)}')
+
+        # For local testing with DynamoDB Local
+        if 'LOCAL_TESTING' in os.environ:
+            endpoint_url = os.environ.get('AWS_ENDPOINT_URL')
+            logger.info(f'Connecting to DynamoDB at: {endpoint_url}')
+            dynamodb = boto3.resource('dynamodb',
+                                    endpoint_url=os.environ.get('AWS_ENDPOINT_URL'),
+                                    region_name='us-east-1',
+                                    aws_access_key_id='dummy',
+                                    aws_secret_access_key='dummy')
+        else:
+            dynamodb = boto3.resource('dynamodb')
+        
+        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+        # Log before DynamoDB operation
+        logger.info('Attempting to scan DynamoDB table')
+
         response = table.scan()
         items = response.get('Items', [])
         # print(response['Items'])
         sorted_items = sorted(items, key=lambda x: x['highScore'], reverse=True)
+        result_json = json.dumps(sorted_items, cls=DecimalEncoder)
         
+        # Log the response (be careful with sensitive data in production)
+        logger.info(f'DynamoDB response sorted : {result_json}')
+
         return {
             'statusCode': 200,
             'headers': {
@@ -37,9 +54,10 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Origin': '*'
             },
              # Use the custom encoder here
-            'body': json.dumps(sorted_items, cls=DecimalEncoder)
+            'body': result_json
         }
     except Exception as e:
+        logger.error(f'Error occurred: {str(e)}', exc_info=True)
         return {
             'statusCode': 500,
             'headers': {
--- lambda/readme.md
+++ lambda/readme.md
@@ -16,7 +16,7 @@ python setup_local.py
 3. Test Lambda function:
 
 ```shell
-LOCAL_TESTING=true DYNAMODB_TABLE=high-scores-table python-lambda-local -f lambda_handler -t 5 app.py event.json
+LOCAL_TESTING=true DYNAMODB_TABLE=high-scores-table AWS_ENDPOINT_URL=http://localhost:4566  python-lambda-local -f lambda_handler -t 5 app.py event.json
 ```
 
 4. To verify table contents:
@@ -24,6 +24,14 @@ LOCAL_TESTING=true DYNAMODB_TABLE=high-scores-table python-lambda-local -f lambd
 ```shell
 python local_list_query.py
 ```
+
+5. Run in SAM
+
+```shell
+sam local start-api --docker-network lambda-local
+```
+
+
 
 5. To stop LocalStack:
 
